ff33c22d1b60504c5dce2b73f80b9a0a50b44298
Andrii Mulyk <amulyk@lectrum.io>
2019-07-05 14:23:00 +0400

    feat(source): Add pathchValue method

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -33,8 +33,10 @@ export class CartComponent implements OnInit {
   }
 
   setDefault() {
-    this.checkoutForm.get('name').setValue('John Doe');
-    this.checkoutForm.get('address').setValue('Kyiv');
+    this.checkoutForm.patchValue({
+      name: 'John Doe',
+      address: 'Kyiv'
+    });
   }
 
   ngOnInit() {
