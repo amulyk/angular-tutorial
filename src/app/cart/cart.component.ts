2d848b69beadfc47e5bd4ca35a1f7e38bb704233
Andrii Mulyk <amulyk@lectrum.io>
2019-07-05 16:23:45 +0400

    feat(source): Add cross field validation

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
-  FormControl,
+  FormControl, FormGroup,
   Validators
 } from '@angular/forms';
 
@@ -23,15 +23,41 @@ export class CartComponent implements OnInit {
     this.items = this.cartService.getItems();
 
     this.checkoutForm = this.formBuilder.group({
-      name: ['', [this.forbiddenName(), Validators.minLength(2)]],
+      name: ['', [this.forbiddenName(), Validators.minLength(4)]],
       address: this.formBuilder.group({
         street: '',
         city: '',
         state: '',
         zip: ''
+      }, {
+        validators: this.crossValidation
       })
     });
   }
+
+  static isZipValid(zip) {
+    return zip.length < 3;
+  }
+
+  static isCityValid(city) {
+    return city && city[0].toLowerCase() === 'a';
+  }
+
+  crossValidation(formGroup) {
+    const zip = formGroup.get('zip').value;
+    const zipStatus = CartComponent.isZipValid(zip);
+
+    const city = formGroup.get('city').value;
+    const cityStatus = CartComponent.isCityValid(city);
+
+    const validationResult = {
+      zipStatus,
+      cityStatus
+    };
+
+    return validationResult.zipStatus && validationResult.cityStatus ? null : validationResult;
+  }
+
 
   forbiddenName() {
     return (formControl) => {
@@ -55,6 +81,18 @@ export class CartComponent implements OnInit {
 
   get name() {
     return this.checkoutForm.get('name') as FormControl;
+  }
+
+  get address() {
+    return this.checkoutForm.get('address') as FormGroup;
+  }
+
+  get zip() {
+    return this.checkoutForm.get('address').get('zip') as FormControl;
+  }
+
+  get city() {
+    return this.checkoutForm.get('address').get('city') as FormControl;
   }
 
   ngOnInit() {
