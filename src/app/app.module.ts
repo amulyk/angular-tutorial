1f4261bd5bf09df1aa09e21a4376d5ef56d94c86
Andrii Mulyk <amulyk@lectrum.io>
2019-07-08 18:11:12 +0400

    feat(source): Add simple observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent
+    ShippingComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +34,7 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
+      { path: 'currency', component: CurrencyComponent },
     ]),
     ReactiveFormsModule
   ],
